85cdcbfc93d0910086dcd68ac8d0cc3d8d251418
Ismael Ruiz <ismael.ruiz@estudiants.urv.cat>
2023-12-19 21:56:46 +0100

    Small Doom final

--- app/src/main/java/com/example/textura/MainActivity.java
+++ app/src/main/java/com/example/textura/MainActivity.java
@@ -27,24 +27,15 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends Activity{
 
     private static final int PERMISSION_REQUEST_CODE = 1;
-
 
     private GLSurfaceView glView;   // Use GLSurfaceView
     private MyGLRenderer myGLRenderer;
     private final float TOUCH_SCALE_FACTOR = 180.0f / 720;
     private float previousX;
     private float previousY;
-
-    private SensorManager sensorManager;
-    private Sensor gyroscopeSensor;
-    private float[] angularSpeed = new float[3]; // Velocidades angulares en los ejes X, Y, Z
-    float[] rotationMatrix = new float[9]; // Matriz de rotación
-    float[] gravity = new float[]{0f, 9.80665f, 0f};
-    float[] magnetometerReading = new float[3];
-    private float[] orientationValues = new float[3]; // Orientación en los ejes azimut, inclinación, y balanceo
 
 
     // Call back when the activity is started, to initialize the view
@@ -55,17 +46,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         glView.setRenderer(myGLRenderer=new MyGLRenderer(this)); // Use a custom renderer
         this.setContentView(glView);                // This activity sets to GLSurfaceView
 
-        // Inicializar el SensorManager
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
-        // Verificar si el dispositivo tiene giroscopio
-        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        if (gyroscopeSensor == null) {
-            // El dispositivo no tiene giroscopio, muestra un mensaje o toma alguna acción
-            Toast.makeText(this, "El dispositivo no tiene giroscopio", Toast.LENGTH_SHORT).show();
-            finish(); // Cierra la actividad
-        }
-
     }
 
     // Call back when the activity is going into the background
@@ -73,7 +53,6 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
         glView.onPause();
-        sensorManager.unregisterListener(this);
     }
 
     // Call back after onPause()
@@ -81,16 +60,6 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onResume() {
         super.onResume();
         glView.onResume();
-        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if (accelerometer != null) {
-            sensorManager.registerListener(this, accelerometer,
-                    SensorManager.SENSOR_DELAY_GAME, SensorManager.SENSOR_DELAY_UI);
-        }
-        Sensor magneticField = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        if (magneticField != null) {
-            sensorManager.registerListener(this, magneticField,
-                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
-        }
     }
 
     @Override
@@ -123,7 +92,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         previousY = y;
         return true;
     }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event){
 
@@ -162,43 +130,15 @@ public class MainActivity extends Activity implements SensorEventListener {
             case KeyEvent.KEYCODE_SPACE:
                 myGLRenderer.jump();
                 return true;
+            case KeyEvent.KEYCODE_N:
+                myGLRenderer.camera();
+                return true;
+            case KeyEvent.KEYCODE_B:
+                myGLRenderer.hardmode();
+                return true;
         }
 
         return super.onKeyDown(keyCode, event);
 
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            System.arraycopy(event.values, 0, angularSpeed,
-                    0, angularSpeed.length);
-
-            updateOrientationAngles();
-
-            //myGLRenderer.updateCamera(angularSpeed);
-        } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, magnetometerReading,
-                    0, magnetometerReading.length);
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public void updateOrientationAngles() {
-        // Update rotation matrix, which is needed to update orientation angles.
-        SensorManager.getRotationMatrix(rotationMatrix, null,
-                angularSpeed, magnetometerReading);
-
-        // "rotationMatrix" now has up-to-date information.
-
-        SensorManager.getOrientation(rotationMatrix, angularSpeed);
-
-        Log.d("Gyroscope", "Valores : "+ Arrays.toString(angularSpeed));
-
-        // "orientationAngles" now has up-to-date information.
     }
 }
--- app/src/main/java/com/example/textura/MyGLRenderer.java
+++ app/src/main/java/com/example/textura/MyGLRenderer.java
@@ -8,12 +8,10 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
 import android.util.Log;
-import android.view.MotionEvent;
-
-import java.io.Console;
 
 /**
  * OpenGL Custom renderer used with GLSurfaceView
@@ -23,31 +21,30 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     private float width, height;
 
-
     Context context;   // Application's context
-    Triangle triangle;
 
-    Wall[] walls = new Wall[4];
-    float[] angular = new float[3];
+    Hud hud;
+    FaceHud face;
+    boolean otherCamera=false;
+    boolean hardmode=false;
+    MediaPlayer doomMusic;
+    float[] fogColor = {0.8f, 0.8f, 0.8f, 1f};
+
 
     private Light light;
-    Object3D object3D;
-
-    TextureCube textureCube;
-    float angle = 0;
+    Object3D[] object3D;
     public float zCam=0;
-
     public float xCamPos=0;
-    public float yCamPos=0;
-    public float zCamPos=3;
+    public float yCamPos=1;
+    public float zCamPos=0;
     public float xCamFront=0;
     public float yCamFront=0;
-    public float zCamFront=-1; // Ejemplo de posición inicial de la cámara
+    public float zCamFront=0; // Ejemplo de posición inicial de la cámara
 
     private float cameraSpeed = 0.1f;
 
     public float pitch = 0.0f;
-    public float yaw = -90.0f;
+    public float yaw = 0.0f;
 
     private static boolean isJumping = false;
     private static float jumpHeight = 1.0f;
@@ -59,38 +56,32 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     // Constructor with global application context
     public MyGLRenderer(Context context) {
-        this.object3D = new Object3D(context, R.raw.mapaisma);
+        this.object3D = new Object3D[30];
+        this.object3D[0] = new Object3D(context, R.raw.suelo);
+        this.object3D[1] = new Object3D(context, R.raw.salaprincipal);
+        this.object3D[2] = new Object3D(context, R.raw.salafinal);
+        this.object3D[3] = new Object3D(context, R.raw.corazon);
+        this.object3D[4] = new Object3D(context, R.raw.door0);
+        this.object3D[5] = new Object3D(context, R.raw.door1);
+        this.object3D[6] = new Object3D(context, R.raw.door2);
+        this.object3D[7] = new Object3D(context, R.raw.door3);
+        this.object3D[8] = new Object3D(context, R.raw.door4);
+        this.object3D[9] = new Object3D(context, R.raw.door5);
+        this.object3D[10] = new Object3D(context, R.raw.door6);
+        this.object3D[11] = new Object3D(context, R.raw.bird);
+        this.object3D[12] = new Object3D(context, R.raw.mountain);
+        this.object3D[13] = new Object3D(context, R.raw.banana);
+        this.object3D[14] = new Object3D(context, R.raw.pressn);
+        this.object3D[15] = new Object3D(context, R.raw.sky);
+        this.object3D[16] = new Object3D(context, R.raw.roof);
+        doomMusic = MediaPlayer.create(context, R.raw.doomtheme);
+
         this.context = context;
-        this.triangle = new Triangle();
-        this.textureCube = new TextureCube();
-        for (int i = 0; i<4; i++){
-            this.walls[i] = new Wall();
-        }
     }
 
     // Call back when the surface is first created or re-created
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Set color's clear-value to black
-        gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
-        gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
-        gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
-        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
-        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
-        gl.glEnable(GL10.GL_BLEND);
-        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
-        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
-        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
-
-        gl.glEnable(GL10.GL_LIGHTING);
-
-        // Enable Normalize
-        gl.glEnable(GL10.GL_NORMALIZE);
-        // You OpenGL|ES initialization code here
-        // ......
 
         light = new Light(gl, GL10.GL_LIGHT0);
         light.setPosition(new float[]{0.0f, 0f, 1, 0.0f});
@@ -98,9 +89,25 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         light.setAmbientColor(new float[]{0.1f, 0.1f, 0.1f});
         light.setDiffuseColor(new float[]{1, 1, 1});
 
-        this.object3D.loadTexture(gl,context, R.raw.brick);
-        // You OpenGL|ES initialization code here
-        // ......
+        this.object3D[0].loadTexture(gl,context, R.raw.floortexture);
+        this.object3D[1].loadTexture(gl,context, R.raw.bluewall);
+        this.object3D[2].loadTexture(gl,context, R.raw.graywall);
+        this.object3D[3].loadTexture(gl,context, R.raw.ventricalsbase);
+        for(int i=4; i<11; i++) {
+            this.object3D[i].loadTexture(gl, context, R.raw.doordoom);
+        }
+        this.object3D[11].loadTexture(gl,context, R.raw.texturebird);
+        this.object3D[12].loadTexture(gl,context, R.raw.grass);
+        this.object3D[13].loadTexture(gl,context, R.raw.bananatextu);
+        this.object3D[14].loadTexture(gl,context, R.raw.texturepressn);
+        this.object3D[15].loadTexture(gl,context, R.raw.skytexture);
+        this.object3D[16].loadTexture(gl,context, R.raw.woodfloor);
+
+        this.hud = new Hud();
+        this.face = new FaceHud();
+
+        doomMusic.setLooping(true);
+        doomMusic.start();
 
     }
 
@@ -126,52 +133,154 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         gl.glLoadIdentity();                 // Reset
     }
 
+    int ticks=0;
+    float posDoor=0;
+    boolean door_down=true;
     // Call back to draw the current frame.
     @Override
     public void onDrawFrame(GL10 gl) {
         Log.d("CameraPosition", "X: " + xCamPos + ", Y: " + yCamPos + ", Z: " + zCamPos);
         // Clear color and depth buffers using clear-value set earlie
 
+        setPerspectiveProjection(gl);
+
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
 
-        updateCameraVectors();
+        if(otherCamera) {
+            GLU.gluLookAt(gl, -8, 10, 34, -5, 20, 80, 0f, 1f, 0f);
 
-        GLU.gluLookAt(gl, xCamPos, yCamPos, zCamPos, xCamPos+xCamFront, yCamPos+yCamFront, zCamPos+zCamFront, 0f, 1f, 0f);
+        }else {
 
-        gl.glPushMatrix();// Reset model-view matrix ( NEW )
-        object3D.draw(gl);                   // Draw triangle ( NEW )
-        gl.glPopMatrix();
+            updateCameraVectors();
 
+            GLU.gluLookAt(gl, xCamPos, yCamPos, zCamPos, xCamPos + xCamFront, yCamPos + yCamFront, zCamPos + zCamFront, 0f, 1f, 0f);
+        }
+
+        if((hardmode || zCamPos < -7.5) && !otherCamera){
+            gl.glEnable(GL10.GL_FOG);
+            gl.glFogf(GL10.GL_FOG_MODE, GL10.GL_LINEAR);
+            gl.glFogfv(GL10.GL_FOG_COLOR, fogColor, 0);
+            gl.glFogf(GL10.GL_FOG_START, 2.0f);  // Ajusta la distancia inicial de la niebla
+            gl.glFogf(GL10.GL_FOG_END, 10.0f);
+        }else{
+            gl.glDisable(GL10.GL_FOG);
+        }
+
+
+        for (int i = 0; i<4; i++){
+            gl.glPushMatrix();// Reset model-view matrix ( NEW
+            object3D[i].draw(gl);                // Draw triangle ( NEW )
+            gl.glPopMatrix();
+        }
+
+
+        for(int i = 4; i<10; i++) {
+            gl.glPushMatrix();
+            if (ticks == 2) {
+                if (door_down) {
+                    posDoor += 0.01;
+                } else {
+                    posDoor -= 0.01;
+                }
+                if (posDoor < -2 || posDoor > 0) door_down = !door_down;
+                ticks = 0;
+            }
+            gl.glTranslatef(0, posDoor, 0);
+            object3D[i].draw(gl);                // Draw triangle ( NEW )
+            gl.glPopMatrix();
+        }
+        ticks++;
 
         gl.glPushMatrix();
-        gl.glTranslatef(0.0f, 0.0f, 10.0f);
-        gl.glRotatef(180, 0, 1,0);
-        gl.glTranslatef(0.0f, 0.0f, 0.0f);
-        walls[0].draw(gl);
+        object3D[10].draw(gl);
         gl.glPopMatrix();
+
+        for(int i=11; i<17; i++){
+            gl.glPushMatrix();
+            object3D[i].draw(gl);
+            gl.glPopMatrix();
+        }
+
+        gl.glDisable(GL10.GL_FOG);
+///////////////////////////////////////////////////////////////////////
+        //gHUD, Cara, Menu
+        if(otherCamera) return;
+        setOrthographicProjection(gl);
+        hud.loadTexture(gl, context);
 
         gl.glPushMatrix();
-        gl.glTranslatef(-1.0f, 0.0f, 9.0f);
-        gl.glRotatef(90, 0, 1,0);
-        gl.glTranslatef(0.0f, 0.0f, 0.0f);
-        walls[1].draw(gl);
+        gl.glScalef(5.0f, 1,1);
+        gl.glTranslatef(0.0f, -3.0f, 0.0f);
+        hud.draw(gl);
         gl.glPopMatrix();
 
+        face.loadTexture(gl, context);
         gl.glPushMatrix();
-        gl.glTranslatef(1.0f, 0.0f, 9.0f);
-        gl.glRotatef(-90, 0, 1,0);
-        gl.glTranslatef(0.0f, 0.0f, 0.0f);
-        walls[3].draw(gl);
+        gl.glScalef(0.53f,0.99f,1f);
+        gl.glTranslatef(0.0f, -3.1f, 0.0f);
+        face.draw(gl);
         gl.glPopMatrix();
 
-
+        Log.d("CameraLock", "X: " + xCamFront + ", Y: " + yCamFront + ", Z: " + zCamFront);
+        Log.d("HudPosition", "X: " + xCamPos+xCamFront + ", Y: " + yCamPos+yCamFront + ", Z: " + zCamPos+zCamFront);
 
        /* currentTime = System.nanoTime();
         System.out.println((currentTime - lastTime) / 1000000);
         lastTime = currentTime;*/
 
     }
+
+    private void setPerspectiveProjection(GL10 gl) {
+
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Set color's clear-value to black
+        gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+        gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+        gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+        gl.glEnable(GL10.GL_BLEND);
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+
+        gl.glEnable(GL10.GL_LIGHTING);
+
+        // Enable Normalize
+        gl.glEnable(GL10.GL_NORMALIZE);
+
+        gl.glClearDepthf(1.0f);            // Set depth's clear-value to farthest
+        gl.glEnable(GL10.GL_DEPTH_TEST);   // Enables depth-buffer for hidden surface removal
+        gl.glDepthFunc(GL10.GL_LEQUAL);    // The type of depth testing to do
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);  // nice perspective view
+        gl.glShadeModel(GL10.GL_SMOOTH);   // Enable smooth shading of color
+        gl.glDisable(GL10.GL_DITHER);      // Disable dithering for better performance
+        gl.glDepthMask(true);  // disable writes to Z-Buffer
+
+        gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+        gl.glLoadIdentity();                 // Reset projection matrix
+
+        // Use perspective projection
+        GLU.gluPerspective(gl, 60, (float) width / height, 0.1f, 100.f);
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);  // Select model-view matrix
+        gl.glLoadIdentity();
+    }
+
+    private void setOrthographicProjection(GL10 gl) {
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        gl.glOrthof(-5,5,-4,4,-5,5);
+        gl.glDepthMask(false);  // disable writes to Z-Buffer
+        gl.glDisable(GL10.GL_DEPTH_TEST);  // disable depth-testing
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+    }
+
 
     public float getHeight() {
         return this.height;
@@ -191,24 +300,19 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
 
     public void movePOVForward() {
+        if(otherCamera) return;
         xCamPos += cameraSpeed * xCamFront;
         zCamPos += cameraSpeed * zCamFront;
     }
 
     public void movePOVBackward() {
+        if(otherCamera) return;
         xCamPos -= cameraSpeed * xCamFront;
         zCamPos -= cameraSpeed * zCamFront;
     }
 
     public void movePOVLeft() {
-        float strafeX = (float)Math.cos(Math.toRadians(yaw - 90.0f));
-        float strafeZ = (float)Math.sin(Math.toRadians(yaw - 90.0f));
-
-        xCamPos -= strafeX * cameraSpeed;
-        zCamPos -= strafeZ * cameraSpeed;
-    }
-
-    public void movePOVRight() {// Calcula la dirección perpendicular a la vista de la cámara
+        if(otherCamera) return;
         float strafeX = (float)Math.cos(Math.toRadians(yaw - 90.0f));
         float strafeZ = (float)Math.sin(Math.toRadians(yaw - 90.0f));
 
@@ -216,25 +320,39 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         zCamPos += strafeZ * cameraSpeed;
     }
 
+    public void movePOVRight() {// Calcula la dirección perpendicular a la vista de la cámara
+        if(otherCamera) return;
+        float strafeX = (float)Math.cos(Math.toRadians(yaw - 90.0f));
+        float strafeZ = (float)Math.sin(Math.toRadians(yaw - 90.0f));
+
+        xCamPos -= strafeX * cameraSpeed;
+        zCamPos -= strafeZ * cameraSpeed;
+    }
+
     public void moveCameraUp() {
+        if(otherCamera) return;
         pitch += 1.0f;
         if (pitch > 89.0f) pitch = 89.0f;
     }
 
     public void moveCameraDown() {
+        if(otherCamera) return;
         pitch -= 1.0f;
         if (pitch < -89.0f) pitch = -89.0f;
     }
 
     public void moveCameraLeft() {
+        if(otherCamera) return;
         yaw -= 1.0f;
     }
 
     public void moveCameraRight() {
+        if(otherCamera) return;
         yaw += 1.0f;
     }
 
     public void jump() {
+        if(otherCamera) return;
         if (!isJumping){
             isJumping = true;
             jumpVelocity = 0.1f;
@@ -242,6 +360,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void updateCameraVectors() {
+        if(otherCamera) return;
         float newFrontx = (float) (cos(Math.toRadians(yaw)) * cos(Math.toRadians(pitch)));
         float newFronty = (float) sin(Math.toRadians(pitch));
         float newFrontz = (float) (sin(Math.toRadians(yaw)) * cos(Math.toRadians(pitch)));
@@ -259,12 +378,24 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
             jumpVelocity -= gravity; // Aplicar gravedad durante el salto
 
             // Verificar si ha alcanzado el suelo
-            if (yCamPos <= 0.0f) {
-                yCamPos = 0.0f;
+            if (yCamPos <= 1.0f) {
+                yCamPos = 1.0f;
                 isJumping = false;
                 jumpVelocity = 0.0f;
             }
         }
     }
 
+    public void camera() {
+        if(otherCamera){
+            otherCamera=false;
+        }else{
+            otherCamera=true;
+        }
+
+    }
+
+    public void hardmode() {
+        hardmode = !hardmode;
+    }
 }
